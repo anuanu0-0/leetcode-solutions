7b91b6bc6dbce18a635fab50555ade87964074e3
Anupam Srivastava <60839849+anuanu0-0@users.noreply.github.com>
2020-08-28 19:59:47 +0530

    Update FirstBadVersion.cpp

--- FirstBadVersion.cpp
+++ FirstBadVersion.cpp
@@ -1,6 +1,8 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
+
+//  First Version
 class Solution {
 public:
     int firstBadVersion(int n) {
@@ -16,5 +18,28 @@ public:
             }
         }
         return high;
+    }
+};
+
+
+//  Another Version
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1, high = n;
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+            if(isBadVersion(mid) == false)
+                low = mid+1;
+            else if(isBadVersion(mid) == true)
+                high = mid-1;
+            else {
+                if(mid == 0 || isBadVersion(mid) != isBadVersion(mid-1))
+                    return mid;
+                else 
+                    high = mid-1;
+            }
+        }
+        return low;
     }
 };
